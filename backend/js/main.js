fd7370d449634b0d3a5c806bbb6a53729fe2c196
Jhennyfer Kamilli Moura Rodrigues <jhennyferkamilli5@gmail.com>
2026-05-25 21:35:10 -0300

    second version - library project

--- backend/js/main.js
+++ backend/js/main.js
@@ -1,13 +1,71 @@
 import executeQuery from "./db.js";
 import readCSV from './read.js';
+await process.loadEnvFile('.env');
 
-let fileContent = await readCSV('./csv/livros.csv');
-fileContent.shift();
+function normalize(text) {
+    
+    try {
+        text = text.trim();
+        text = text === "" ? null : text;
+    } catch (error) {
+        text = null;
+    }
 
-let query;
-let params;
-for (let row of fileContent) {
-    row = row.split(';');
+    return text;
+}
+
+async function selectAuthor(row) {
+
+    let query;
+    let param;
+
+    query = `
+        SELECT 
+            id, 
+            name 
+        FROM 
+            authors
+        WHERE 
+            name = ?
+    `;
+
+    param = [normalize(row[1])];
+
+    try {
+        let result = await executeQuery(query, param);
+        return result[0][0].id;
+    } catch (error) {
+        return null;
+    }
+}
+
+async function insertAuthor(row) {
+
+    let query;
+    let param;
+
+    query = `
+        INSERT INTO authors(
+            name 
+        ) VALUES (
+            ?
+        )
+    `;
+
+    param = [normalize(row[1])];
+
+    try {
+        let result = await executeQuery(query, param);
+        return result[0].insertId;
+    } catch (error) {
+        return null;
+    }
+}
+
+async function insertBooks(row, idAuthor) {
+
+    let query;
+    let params;
 
     query = `
         INSERT INTO books(
@@ -26,12 +84,34 @@ for (let row of fileContent) {
     `;
 
     params = [
-        row[2],
-        row[0],
-        row[1],
-        row[4],
-        row[3]
+        normalize(row[2]),
+        normalize(row[0]),
+        idAuthor,
+        normalize(row[4]),
+        normalize(row[3])
     ];
 
     await executeQuery(query, params);
-};
+}
+
+async function main() {
+    let fileContent = await readCSV('./csv/livros.csv');
+    let idAuthor;
+    fileContent.shift();
+
+
+    for (let row of fileContent) {
+
+        row = row.split(';');
+
+        idAuthor = await selectAuthor(row);
+        if (idAuthor) {
+            await insertBooks(row, idAuthor);
+        } else {
+            idAuthor = await insertAuthor(row);
+            await insertBooks(row, idAuthor);
+        }
+    };
+}
+
+await main();
--- backend/sql/library.sql
+++ backend/sql/library.sql
@@ -1,16 +1,30 @@
--- Active: 1779145650504@@127.0.0.1@3306@library
+-- Active: 1778547260903@@127.0.0.1@3306
 DROP DATABASE IF EXISTS library;
 CREATE DATABASE IF NOT EXISTS library;
 USE library;
+
+
+CREATE TABLE IF NOT EXISTS authors(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(250) NOT NULL UNIQUE 
+)
 
 CREATE TABLE IF NOT EXISTS books(
     id INT PRIMARY KEY AUTO_INCREMENT,
     isbn VARCHAR(16) NOT NULL UNIQUE,
     title VARCHAR(250) NOT NULL,
-    author VARCHAR(150) NOT NULL,
     year CHAR(4),
-    pages INT
+    pages INT,
+    author INT NOT NULL,
+
+    FOREIGN KEY (author) REFERENCES authors(id) 
 );
+
+-- INSERT INTO authors(
+--     name 
+-- ) VALUES (
+--     ?
+-- );
 
 -- INSERT INTO books(
 --     isbn,
@@ -24,4 +38,12 @@ CREATE TABLE IF NOT EXISTS books(
 --     ?,
 --     ?
 -- );
+
+-- SELECT 
+--     id, 
+--     name 
+-- FROM 
+--     authors
+-- WHERE 
+--     name = 'albert';
 
